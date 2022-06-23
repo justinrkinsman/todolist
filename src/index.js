90ac01a118b109994ca4c74e79fee5ff1a56ed35
Justin <justinrkinsman@gmail.com>
2022-06-23 10:11:28 -0300

    Add function to console.log form values

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 class Todo {
-    constructor(task, description, dueDate, priority) {
+    constructor(task, description, dueDate, priority, notes) {
         this.task = task;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority
+        this.notes = notes
     }
 }
 
 const content = document.getElementById('content')
+const task = document.getElementById('task')
+const desc = document.getElementById('description')
+const due = document.getElementById('dueDate')
+const priority = document.getElementById('priority')
+const notes = document.getElementById('notes')
+const submitBtn = document.getElementById('submit')
+submitBtn.addEventListener('click', submission)
 
-let myTodo = new Todo('Sweep', "Sweep kitchen", 'Today', 'Medium')
-console.log(myTodo.task)
+function submission() {
+    console.log(task.value)
+    console.log(desc.value)
+    console.log(due.value)
+    console.log(priority.value)
+    console.log(notes.value)
+}
+
+function clearForm(){
+
+}
+
+let form = document.getElementById('myForm');
+function handleForm(event) { event.preventDefault(); }
+form.addEventListener('submit', handleForm)
+//let myTodo = new Todo('Sweep', "Sweep kitchen", 'Today', 'Medium')
